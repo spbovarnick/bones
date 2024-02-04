0b72ff02defb4c0f4ff045a6f861d4585cef370d
spbovarnick <spbovarnick@gmail.com>
2024-02-04 14:07:59 -0800

    post cards styled

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,5 +1,6 @@
 import { sanityFetch } from "@/utils/api/sanityFetch"
 import ClientImg from "../components/ClientImg"
+import styles from "./posts.module.css"
 
 
 export default async function Posts(){
@@ -18,16 +19,23 @@ export default async function Posts(){
 
 
   return (
-    <>
-      {posts.map((post, index) => (
-        <div key={index}>
-         {post.title}
-         <ClientImg 
-          img={post.mainImage}
-          sizes={"100vw"}
-         />
-        </div>
-      ))}
-    </>
+    <div>
+      <h1>Image Component In Action</h1>
+      <hr/>
+      <section className={styles.posts}>
+        {posts.map((post, index) => (
+          <div key={index} className={styles.card}>
+            <h3 className={styles.cardTitle}>{post.title}</h3>
+          <div >
+            <ClientImg 
+              img={post.mainImage}
+              sizes={"100vw"}
+              
+            />
+          </div>
+          </div>
+        ))}
+      </section>
+    </div>
   )
 }
